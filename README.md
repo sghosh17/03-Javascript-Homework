3ae50931ef3720494d81cbf448727522d93e6d50
Sampreeti Das <sampreetighosh@gmail.com>
2021-11-10 13:13:21 +0000

    Updated the files of the password generation

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # 03-Javascript-Homework
+
+
+## Week 03 Homework
+
+This project involves enhancing an existing code so that the code generates a password based on the various criterias selected. As part of enhancing the code, I have updated the existing script.js file by writing a new fuction to generate a random password. 
+
+Within the function I did the follwing steps:
+
+* Used prompts to take the password length as user input.
+
+* Used confirm dialogs to confirm the user inputs for the various criterias.
+
+* Based on the various options selected by the user, a number of operations are performed -
+
+            *   Validation on the password length.
+            *   Concatenation based on the various combinations of the criterias selected.   
+ 
+* At the end a random password is generated using the random function.
+
+* Displaying the generated password in the designated textarea of the index.html file.
+
+
+### Link to the deployed application
+
+Click [here]( https://sghosh17.github.io/03-Javascript-Homework/) to access the application.
--- script.js
+++ script.js
@@ -1,14 +1,139 @@
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var pass_lenght;
+var confirm_no;
+var confirm_char;
+var confirm_upper;
+var confirm_lower;
+
+
+// Special characters 
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", 
+              ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+// Numeric characters
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// Alphabetical characters
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+         "u", "v", "w", "x", "y", "z"];
+         
+
+alpha_upper=(alpha.map(function(x){
+  return x.toUpperCase();
+}));
+
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  passwordText.value = "";
 
+  var password = generatePassword();
+  
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+
+  var choices;
+     // Asks for user input
+     pass_lenght = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
+     // First if statement for user validation 
+    if (!pass_lenght) {
+      alert("This needs a value");
+  } else if (pass_lenght < 8 || pass_lenght > 128) {
+      // Validates user input
+      // Start user input prompts
+      generatePassword();
+
+  }else {
+    // User Input
+    confirm_no = confirm("Will this contain numbers?");
+    confirm_char = confirm("Will this contain special characters?");
+    confirm_upper = confirm("Will this contain Uppercase letters?");
+    confirm_lower = confirm("Will this contain Lowercase letters?");
+  };
+
+  // Else if all options are false
+  if (!confirm_char && !confirm_no && !confirm_upper && !confirm_lower) {
+    choices = alert("You must choose a criteria!");
+
+  }
+  // Else if all options are true
+  else if (confirm_char && confirm_no && confirm_upper && confirm_lower) {
+
+    choices = character.concat(number, alpha, alpha_upper);
+    //console.log('Inside 4');
+  }
+
+  // Else if any three options are true
+  else if (confirm_char && confirm_no && confirm_upper) {
+    //console.log('Inside 3');
+    choices = character.concat(number, alpha_upper);
+  }
+  else if (confirm_char && confirm_no && confirm_lower) {
+    choices = character.concat(number, alpha);
+  }
+  else if (confirm_char && confirm_lower && confirm_upper) {
+    choices = character.concat(alpha, alpha_upper);
+  }
+  else if (confirm_no && confirm_lower && confirm_upper) {
+    choices = number.concat(alpha, alpha_upper);
+  }
+
+  // Else if any two options are true
+ else if (confirm_char && confirm_no) {
+  //console.log('Inside 2');
+    choices = character.concat(number);
+
+  } else if (confirm_char && confirm_lower) {
+    choices = character.concat(alpha);
+
+  } else if (confirm_char && confirm_upper) {
+    choices = character.concat(alpha_upper);
+
+  }else if (confirm_lower && confirm_no) {
+    choices = alpha.concat(number);
+
+  } else if (confirm_lower && confirm_upper) {
+    choices = alpha.concat(alpha_upper);
+
+  } else if (confirm_no && confirm_upper) {
+    choices = number.concat(alpha_upper);
+  }
+
+ // Else if only any one option is true
+ else if (confirm_char) {
+  choices = character;
+}
+else if (confirm_no) {
+  choices = number;
+}
+else if (confirm_lower) {
+  choices = alpha;
+}
+// Created space variable to fill uppercase conversion
+else if (confirm_upper) {
+  //choices = space.concat(alpha_upper);
+  choices = alpha_upper;
+};
+
+  // Random selection
+  var space = [];
+  for (var i = 0; i < pass_lenght; i++) {
+    
+    var choices_random = choices[Math.floor(Math.random() * choices.length)]; //choices.lenght=((max - min)+min)
+    space.push(choices_random);
+  }
+  
+// This joins the password array and converts it to a string
+    var ps = space.join("");
+    console.log(ps);
+    return ps;
+
+}
